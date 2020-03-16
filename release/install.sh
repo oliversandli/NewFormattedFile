be5477e5b7cdf829f308e47f3651976e0ef73f5b
oliversandli <56598098+oliversandli@users.noreply.github.com>
2020-03-16 16:42:27 -0500

    Update install.sh

--- release/install.sh
+++ release/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Compiling source & installing..."
-g++ nffv2.cpp -o /usr/local/bin/nff
+g++ nff.cpp -o /usr/local/bin/nff
 echo "Creating config directory..."
 mkdir ~/.config/nff/
 echo "Copying default templates..."
+mkdir ~/.config/nff/templates/
 cp templates ~/.config/nff/templates/
 echo "DONE."
