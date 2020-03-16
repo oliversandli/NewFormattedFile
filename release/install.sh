4e3730ee1b26458be3b8e37d82ccc87f1826944e
oliversandli <56598098+oliversandli@users.noreply.github.com>
2020-03-16 16:56:04 -0500

    Fix template location

--- release/install.sh
+++ release/install.sh
@@ -5,6 +5,5 @@ g++ nff.cpp -o /usr/local/bin/nff
 echo "Creating config directory..."
 mkdir ~/.config/nff/
 echo "Copying default templates..."
-mkdir ~/.config/nff/templates/
-cp  -r templates ~/.config/nff/templates/
+cp -r templates ~/.config/nff/templates/
 echo "DONE"
