a614034ea5dc38e21c19016f55b45b104f08b2c2
oliversandli <56598098+oliversandli@users.noreply.github.com>
2020-03-17 16:31:21 -0500

    Update install.sh

--- release/install.sh
+++ release/install.sh
@@ -7,11 +7,13 @@ if [ $XDG_CONFIG_HOME ]
 then
   mkdir $XDG_CONFIG_HOME/nff/
   cp -r templates $XDG_CONFIG_HOME/nff/templates/
+  echo "Installed templates in '$XDG_CONFIG_HOME/nff/templates/'"
 else
   if [ ~/.config ]
   then
     mkdir ~/.config/nff/
     cp -r templates ~/.config/nff/templates/
+    echo "Installed templates in '~/.config/nff/templates/'"
   else
     echo "Error: cannot find '$XDG_CONFIG_HOME' or '~/.config'"
     echo "Create templates directory manually or uninstall nff."
