1d6546f8ca7c9ee462071e81bf5d2314afbbd3fe
oliversandli <56598098+oliversandli@users.noreply.github.com>
2020-03-17 16:13:30 -0500

    Update install.sh

--- release/install.sh
+++ release/install.sh
@@ -3,17 +3,17 @@
 echo "Compiling source & installing..."
 g++ nff.cpp -o /usr/local/bin/nff
 echo "Creating config directory and templates..."
-if [ $XDG_DATA_PATH ]
+if [ $XDG_CONFIG_HOME ]
 then
-  mkdir $XDG_DATA_PATH/nff/
-  cp -r templates $XDG_DATA_PATH/nff/templates/
+  mkdir $XDG_CONFIG_HOME/nff/
+  cp -r templates $XDG_CONFIG_HOME/nff/templates/
 else
   if [ ~/.config ]
   then
     mkdir ~/.config/nff/
     cp -r templates ~/.config/nff/templates/
   else
-    echo "Error: cannot find '$XDG_DATA_PATH' or '~/.config'"
+    echo "Error: cannot find '$XDG_CONFIG_HOME' or '~/.config'"
     echo "Create templates directory manually or uninstall nff."
     exit 1
   fi
