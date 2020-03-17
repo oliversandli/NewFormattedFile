7ce972cd76c7a0e1512af01cdda117d11ae53000
oliversandli <56598098+oliversandli@users.noreply.github.com>
2020-03-17 13:56:47 -0500

    Add XDG support to install.sh

--- release/install.sh
+++ release/install.sh
@@ -2,8 +2,20 @@
 
 echo "Compiling source & installing..."
 g++ nff.cpp -o /usr/local/bin/nff
-echo "Creating config directory..."
-mkdir ~/.config/nff/
-echo "Copying default templates..."
-cp -r templates ~/.config/nff/templates/
+echo "Creating config directory and templates..."
+if [ $XDG_DATA_PATH ]
+then
+  mkdir $XDG_DATA_PATH/nff/
+  cp -r templates $XDG_DATA_PATH/nff/templates/
+else
+  if [ ~/.config ]
+  then
+    mkdir ~/.config/nff/
+    cp -r templates ~/.config/nff/templates/
+  else
+    echo "Error: cannot find '$XDG_DATA_PATH' or '~/.config'
+    echo "Create templates directory manually or uninstall nff."
+    exit 1
+  fi
+fi
 echo "DONE"
