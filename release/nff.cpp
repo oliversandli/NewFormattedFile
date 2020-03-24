1d3e00d84c03594c55b8f1e9580807089b63da81
oliversandli <56598098+oliversandli@users.noreply.github.com>
2020-03-24 10:06:54 -0500

    Update style to snake_case

--- release/nff.cpp
+++ release/nff.cpp
@@ -5,44 +5,44 @@
 #include <string>
 #include <unistd.h>
 
-std::string readFile(std::string fileName) {
-    std::ifstream inFile;
-    inFile.open(fileName);
-    std::stringstream strStream;
-    strStream << inFile.rdbuf();
-    return strStream.str();
+std::string read_file(std::string file_name) {
+    std::ifstream in_file;
+    in_file.open(file_name);
+    std::stringstream str_stream;
+    str_stream << in_file.rdbuf();
+    return str_stream.str();
 }
 
-void writeFile(std::string fileName, std::string fileContents) {
-    std::ofstream out(fileName);
-    out << fileContents;
+void write_file(std::string file_name, std::string file_contents) {
+    std::ofstream out(file_name);
+    out << file_contents;
     out.close();
 }
 
-std::string confDir() {
-    char *XDGPathChar = getenv("XDG_CONFIG_HOME");
-    if (XDGPathChar == NULL) {
+std::string conf_dir() {
+    char *XDG_path_char = getenv("XDG_DATA_HOME");
+    if (XDG_path_char == NULL) {
         struct passwd *pw = getpwuid(getuid());
-        std::string HOMEPath(pw->pw_dir);
-        return HOMEPath + "/.config";
+        std::string home_path(pw->pw_dir);
+        return home_path + "/.config";
     }
-    std::string XDGPath(XDGPathChar);
-    return XDGPath;
+    std::string XDG_path(XDG_path_char);
+    return XDG_path;
 }
 
 int main(int argc, char** argv) {
     if (argc == 2) {
-        std::string newFileName(argv[1]);
-        if ((newFileName == "--help") or (newFileName == "-h")) {
-            std::cout << "New Formatted File v1.1.0\nCreate new file with the format assigned to the file extension.\nSee https://github.com/oliversandli/NewFormattedFile for help/documentation." << std::endl;
+        std::string new_file_name(argv[1]);
+        if ((new_file_name == "--help") or (new_file_name == "-h")) {
+            std::cout << "New Formatted File v1.0.1\nCreate new file with the format assigned to the file extension.\nSee https://github.com/oliversandli/NewFormattedFile for help/documentation." << std::endl;
         } else {
-            std::string confPath(confDir() + "/nff/templates/" + newFileName.substr(newFileName.find(std::string(".")) + 1) + ".conf");
-            std::string confContents(readFile(confPath));
-            if (confContents == "") {
-                std::cout << "Error: no such config: " << confPath << std::endl;
+            std::string conf_path(conf_dir() + "/nff/templates/" + new_file_name.substr(new_file_name.find(".") + 1) + ".conf");
+            std::string conf_contents(read_file(conf_path));
+            if (conf_contents == "") {
+                std::cout << "Error: no such config: " << conf_path << std::endl;
                 return 1;
             } else {
-                writeFile(newFileName, confContents);
+                write_file(new_file_name, conf_contents);
             }
         }
     } else {
